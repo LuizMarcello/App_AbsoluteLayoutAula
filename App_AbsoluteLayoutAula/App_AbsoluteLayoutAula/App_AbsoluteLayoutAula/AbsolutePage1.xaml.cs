d213374dbb955e51c40ae783c4347970ac56d0cc
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-06-19 01:27:28 -0300

    19.06.2020_01:27hs

---
+++ App_AbsoluteLayoutAula/App_AbsoluteLayoutAula/App_AbsoluteLayoutAula/AbsolutePage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_AbsoluteLayoutAula
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AbsolutePage1 : ContentPage
+    {
+        public AbsolutePage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
