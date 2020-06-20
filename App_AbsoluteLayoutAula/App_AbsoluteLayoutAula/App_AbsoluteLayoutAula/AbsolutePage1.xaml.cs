3644a47925176a15c4b3a56debe5ad9097dfcf84
luiz marcello <luizmarcello.vmo@hotmail.com>
2020-06-20 10:21:02 -0300

    20.06.2020_10:20hs

--- App_AbsoluteLayoutAula/App_AbsoluteLayoutAula/App_AbsoluteLayoutAula/AbsolutePage1.xaml.cs
+++ App_AbsoluteLayoutAula/App_AbsoluteLayoutAula/App_AbsoluteLayoutAula/AbsolutePage1.xaml.cs
@@ -15,6 +15,24 @@ namespace App_AbsoluteLayoutAula
         public AbsolutePage1()
         {
             InitializeComponent();
+
+            var layout = new AbsoluteLayout();
+
+            var box1 = new BoxView { Color = Color.Purple };
+
+            layout.Children.Add(box1, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.All);
+
+            var box2 = new BoxView { Color = Color.Yellow };
+
+            layout.Children.Add(box2, new Rectangle(0.5, 0.1, 100, 100), AbsoluteLayoutFlags.PositionProportional);
+
+            var button1 = new Button { Text="Macoratti.net", BackgroundColor = Color.Lime, TextColor=Color.Black };
+
+            layout.Children.Add(button1, new Rectangle(0,1,1,40), AbsoluteLayoutFlags.PositionProportional | 
+                AbsoluteLayoutFlags.WidthProportional);
+
+            Content = layout;
         }
     }
 }
+            
